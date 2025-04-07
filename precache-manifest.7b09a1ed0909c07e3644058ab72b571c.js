cec7040e8c4bf55544a9f9968e76c8c05059465b
YoryiNin <ratapika01@gmail.com>
2025-04-06 22:13:45 -0600

    Updates

---
+++ precache-manifest.7b09a1ed0909c07e3644058ab72b571c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de5b6178966a596132256f86a0e6d08",
+    "url": "/Universidad/index.html"
+  },
+  {
+    "revision": "a6cb5e96a38647c41772",
+    "url": "/Universidad/static/css/2.b0aab389.chunk.css"
+  },
+  {
+    "revision": "99c8ef8c02d769ddf40f",
+    "url": "/Universidad/static/css/main.a771d9f9.chunk.css"
+  },
+  {
+    "revision": "a6cb5e96a38647c41772",
+    "url": "/Universidad/static/js/2.9ffdf070.chunk.js"
+  },
+  {
+    "revision": "0e78cb4eadd47ea0ac070afaa0ebcb79",
+    "url": "/Universidad/static/js/2.9ffdf070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c8ef8c02d769ddf40f",
+    "url": "/Universidad/static/js/main.53f96208.chunk.js"
+  },
+  {
+    "revision": "844d664a2f252d808294",
+    "url": "/Universidad/static/js/runtime-main.3a53b409.js"
+  }
+]);
